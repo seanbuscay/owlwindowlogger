79dfc6bd1bf38792d71e2e94582fd1d0afa8fc9b
unknown <Sean Buscay@.(none)>
2012-03-27 17:55:45 -0400

    Try to fix encoding error by adjusting space under: #encoding:UTF-8

--- jsonlogwrite.py
+++ jsonlogwrite.py
@@ -1,4 +1,5 @@
 #encoding:UTF-8
+
 #@change: for above see http://www.python.org/dev/peps/pep-0263/
 #@change: switch to using simplejson when available attempting to remove erros
 try: import simplejson as json
