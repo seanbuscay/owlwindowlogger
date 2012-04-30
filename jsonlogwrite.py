8f8b875f46aff229d1bffa2c58ad72e353700a00
Sean Buscay <sbuscay@gmail.com>
2012-04-30 18:32:46 -0400

    logging in drupal date friendly format

--- jsonlogwrite.py
+++ jsonlogwrite.py
@@ -5,13 +5,17 @@
 try: import simplejson as json
 except ImportError: import json
 
-from time import localtime, time
+from time import localtime, time, strftime
+import uuid
 def Write(data, logfile):
         logfile = logfile + '.log'
         data['Timestamp'] = int(time())
-        #text = json.dumps(data) #normal
-        #text = json.dumps(data, sort_keys=True, indent=2) #nicely formated
+        data['GUID'] = str(uuid.uuid4())
+        data['date_time_stamp'] = strftime("%d %b %Y - %H:%M:%S")
+        data['ActiveText'] = data['ActiveText'].encode('string_escape')
+        #add more data to make it freindly to Drupal date module
         text = json.dumps(data, separators=(',',':')) #compact
+        print (text)
         f = open(logfile, 'a', )
         f.write (text + "\n")
         f.close()
